7be3fcb5e9293e310667f99e52b873c2367a78c3
powerpizza67695 <scihack.official@gmail.com>
2025-08-04 16:44:35 +0530

    Fixed circular linked list

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -1,102 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
     int data;
     struct node * next_node;
 } Node;
 
-Node * initCircularLinkedList(){
+Node * initCircularLL(){
     Node * empty_node = (Node*) malloc(sizeof(Node));
+    empty_node->data = -1;
     empty_node->next_node = empty_node;
     return empty_node;
 }
 
-void addDataNode(Node * cl, int data){
-    if (cl->next_node == cl){
-        Node * head_node = (Node*) malloc(sizeof(Node));
-        head_node->data = data;
-        head_node->next_node = head_node;
-        cl->next_node = head_node;
-        return;
-    }
-
-    Node * head_node = cl->next_node;
-
-    while (head_node->next_node != cl->next_node)
-    {
-        head_node = head_node->next_node;
-    }
-    
+void append(Node * empty_node, int data){
+    // Time complexity in worse case O(n)
+    // Time complexity in best case O(1)
+    Node * head_node = empty_node->next_node;
+    Node * last_node = head_node;
+    do {
+        last_node = last_node->next_node;
+    } while (head_node != last_node->next_node);
     Node * new_node = (Node*) malloc(sizeof(Node));
     new_node->data = data;
-    new_node->next_node = cl->next_node;
-    head_node->next_node = new_node;
+    last_node->next_node = new_node;
+    if (empty_node != head_node) {
+        new_node->next_node = head_node;
+    }
+    else{
+        new_node->next_node = new_node;
+    }
 }
 
-void printCircularList(Node * cl){
-    Node * head_node = cl->next_node;
-    do{
-        printf("%d\n", head_node->data);
+void printCircularLL(Node * empty_node){
+    // Time complexity in worse case O(n)
+    Node * head_node = empty_node->next_node;
+    do {
+        printf("%d, ", head_node->data);
         head_node = head_node->next_node;
-    } while (cl->next_node != head_node);
+    } while (empty_node->next_node != head_node);
+    printf("\b\b \n");
 }
 
-void insertData(Node * cl, int index, int data){
-    Node * p = cl->next_node;
-    Node * q = p->next_node;
+void delete(Node * empty_node, int index){
+    // Time complexity in worse case O(n)
+    // Time complexity in best case O(1)
+    Node * prev_node = empty_node;
+    Node * to_delete = empty_node->next_node;
     for (int i = 0; i < index; i++){
-        if (cl->next_node == p->next_node){
-            Node * new_node = (Node*) malloc(sizeof(Node));
-            new_node->data = data;
-            new_node->next_node = p->next_node;
-            p->next_node = new_node;
-            return;
-        }
-        p = q;
-        q = q->next_node;
+        to_delete = to_delete->next_node;
+        prev_node = prev_node->next_node;
     }
-    Node * new_node = (Node*) malloc(sizeof(Node));
-    new_node->data = p->data;
-    new_node->next_node = q;
-    p->data = data;
-    p->next_node = new_node;
-}
-
-void deleteData(Node * cl, int idx){
-    Node * head_node = cl->next_node;
-    for (int i = 0; i < idx; i++){
-        if (cl->next_node == head_node->next_node->next_node){
-            Node * to_free = head_node->next_node;
-            head_node->next_node = cl->next_node;
-            free(to_free);
-            return;
-        }
-        head_node = head_node->next_node;
+    if (to_delete->next_node == empty_node->next_node){
+        prev_node->next_node = empty_node->next_node;
+        free(to_delete);
+        return;
     }
-    head_node->data = head_node->next_node->data;
-    Node * to_free = head_node->next_node;
-    head_node->next_node = head_node->next_node->next_node;
+    Node * to_free = to_delete->next_node;
+    to_delete->data = to_delete->next_node->data;
+    to_delete->next_node = to_delete->next_node->next_node;
     free(to_free);
 }
 
+void insert(Node * empty_node, int index, int data){
+    // Time complexity best case O(1)
+    // Time complexity worse case O(n)
+    Node * prev_node = empty_node;
+    Node * insert_at = empty_node->next_node;
+    for (int i = 0; i < index; i++){
+        prev_node = prev_node->next_node;
+        insert_at = insert_at->next_node;
+    }
+    if (insert_at == empty_node->next_node && index != 0){
+        Node * new_node = (Node*) malloc(sizeof(Node));
+        new_node->data = data;
+        new_node->next_node = empty_node->next_node;
+        prev_node->next_node = new_node;
+        return;
+    }
+    Node * new_node = (Node*) malloc(sizeof(Node));
+    new_node->data = insert_at->data;
+    new_node->next_node = insert_at->next_node;
+    insert_at->data = data;
+    insert_at->next_node = new_node;
+}
+
 int main(){
-    Node * cl1 = initCircularLinkedList();
-    // printf("%d\n", cl1);  // empty node's data.
-    addDataNode(cl1, 90);
-    addDataNode(cl1, 78);
-    addDataNode(cl1, 21);
-    addDataNode(cl1, 56);
-    addDataNode(cl1, 78);
-    addDataNode(cl1, 22);
-    printCircularList(cl1);
+    Node * cl1 = initCircularLL();
+    append(cl1, 90);
+    append(cl1, 78);
+    append(cl1, 21);
+    append(cl1, 56);
+    append(cl1, 78);
+    append(cl1, 22);
+    printCircularLL(cl1);
     printf("AFTER INSERTION\n");
-    insertData(cl1, 10, 100);
-    insertData(cl1, 6, 12);
-    printCircularList(cl1);
+    insert(cl1, 10, 100);
+    insert(cl1, 6, 12);
+    printCircularLL(cl1);
     printf("AFTER DELETION\n");
-    deleteData(cl1, 1);
-    printCircularList(cl1);
+    delete(cl1, 1);
+    printCircularLL(cl1);
     return 0;
 }
